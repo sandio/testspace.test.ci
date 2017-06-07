b2fbefafb782fbf43357d0aea51f9598c0da42af
Mark Underseth <marku@s2technologies.com>
2017-06-07 00:47:39 +0000

    adding yml files

--- c9build.sh
+++ c9build.sh
@@ -2,7 +2,9 @@
 
 mkdir -p $HOME/bin
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | tar -zxvf- -C $HOME/bin
-testspace config url http://095d8acfaaf71b4a0f3767e41152620ebdda1b31:@s2.stridespace.com/s2technologies:testspace.test.ci
+testspace config url http://109665cb6fac68609b83f1672a3d334a65895a59:@s2.stridespace.com/s2technologies:testspace.test.ci
 testspace -v
 
-testspace results.xml "standalone#c9.build"
+printenv > printenv.txt
+
+testspace results.xml printenv.txt "standalone#c9.build"
