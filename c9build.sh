127a48a014bf3bc60709d6966efa0bc0652f577f
Mark Underseth <marku@s2technologies.com>
2017-06-09 13:40:44 +0000

    switched to s2-testspace user access token

--- c9build.sh
+++ c9build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $HOME/bin
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | tar -zxvf- -C $HOME/bin
-testspace config url http://109665cb6fac68609b83f1672a3d334a65895a59:@s2.stridespace.com/s2technologies:testspace.test.ci
+testspace config url http://21a2b85afb9b6370c8c15144cad577fa72cf7b78:@s2.stridespace.com/s2technologies:testspace.test.ci
 testspace -v
 
 printenv > printenv.txt
