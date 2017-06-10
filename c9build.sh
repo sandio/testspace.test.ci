4ba9b7dd828ccb10446665bc5dad3255b4550b30
Mark Underseth <marku@s2technologies.com>
2017-06-10 16:39:31 +0000

    added --repo and CI Env Tokens

--- c9build.sh
+++ c9build.sh
@@ -2,9 +2,9 @@
 
 mkdir -p $HOME/bin
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | tar -zxvf- -C $HOME/bin
-testspace config url http://21a2b85afb9b6370c8c15144cad577fa72cf7b78:@s2.stridespace.com/s2technologies:testspace.test.ci
+testspace config url http://$TS_TOKEN:@s2.stridespace.com/s2technologies:testspace.test.ci
 testspace -v
 
 printenv > printenv.txt
 
-testspace results.xml printenv.txt "standalone#c9.build"
+testspace results.xml printenv.txt "standalone#c9.build" --repo git
