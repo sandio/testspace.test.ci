e27a26296b70563d59feacdf10ad222dffffcfdc
Aleksandar Krastev <sandio.mama@gmail.com>
2017-09-19 16:52:47 +0300

    Log content size as metric data

--- ts-time-push.sh
+++ ts-time-push.sh
@@ -8,10 +8,14 @@ CONTENT_SIZE=$(stat -c  '%s' check-size.xml)
 
 NUM_FILES=$(ls -l result*.xml | grep -v ^d | wc -l )
 
+# Log file
 echo "TIMING STATS:" > timing.log
 echo "  Number of files: $NUM_FILES" >> timing.log
 echo "  Size of content(files X 3): $CONTENT_SIZE" >> timing.log
 echo "  $JOB Upload time in seconds, $TIME_DIFF" >> timing.log
+
+# CSV file
 echo "$JOB Upload time, $TIME_DIFF" > timing.csv
+echo "$JOB Content size, $CONTENT_SIZE" >> timing.csv
 
 testspace [$JOB]"timing.log{timing.csv:Tracking Time for $JOB}"
